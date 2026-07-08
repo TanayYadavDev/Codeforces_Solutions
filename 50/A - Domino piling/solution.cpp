bba73c3ad0b7b9dae18bc423a6348631f850f78f
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-07-08 13:06:30 +0530

    Add Domino piling [A] from Codeforces

---
+++ 50/A - Domino piling/solution.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int m , n;
+    cin >> m >> n;
+    int count = 0;
+    count = (m*n)/2;
+    cout << count << '
+';
+    return 0;
+}
