dc085f315438f0373cfb2eecfecf7236664738e4
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-07-08 13:07:57 +0530

    Add Stones on the Table [A] from Codeforces

---
+++ 266/A - Stones on the Table/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (s[i] == s[i + 1]) {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
