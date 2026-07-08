8f00ef0b152a5640dfd478d4c3a25c34f714dd55
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-07-08 13:07:21 +0530

    Add Perfect Root [A] from Codeforces

---
+++ 2185/A - Perfect Root/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+        
+        for (int i = 1; i <= n; i++ ) {
+            cout << i << " ";
+            
+        }
+    }
+    return 0;
+}
