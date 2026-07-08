5c449022d8ba5413e3add4d24d286342251e8164
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-07-08 13:10:16 +0530

    Add Drinks [B] from Codeforces

---
+++ 200/B - Drinks/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n, sum = 0;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        sum += a[i];
+    }
+    double ans = (double) sum / n;
+    cout << ans << "
+";
+    return 0;
+}
