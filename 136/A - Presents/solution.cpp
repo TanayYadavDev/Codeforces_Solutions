0dcd148b8f40a3eff62efb7bc76dadd4556731f3
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-07-08 13:10:23 +0530

    Add Presents [A] from Codeforces

---
+++ 136/A - Presents/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        b[a[i] - 1] = i + 1;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << b[i] << " ";
+    }
+ 
+    return 0;
+}
