16c70c08e0f49b4575a2b76abc448ebdcf3b6bd0
Tanay Singh Yadav <94616913+TanayYadavDev@users.noreply.github.com>
2026-07-10 12:22:00 +0530

    Add Candies and Two Sisters [A] from Codeforces

---
+++ 1335/A - Candies and Two Sisters/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        cout << (n-1) / 2 << "
+";
+    }
+ 
+    return 0;
+}
